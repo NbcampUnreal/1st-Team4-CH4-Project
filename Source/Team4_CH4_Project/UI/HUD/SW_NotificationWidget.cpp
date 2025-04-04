d5593961f28116eba8c2e4a4b4dcf40dec0b3153
nonecalculate <escortbayonet@gmail.com>
2025-04-04 15:27:07 +0900

    Revert "Merge branch 'Dev' into KJS/GameMode"
    This reverts commit bc623fdf95298a7cbea70ae864e8a6b69635663b, reversing
    changes made to 313e3ad198dc055ddf1a434a0ae25954eaf1e871.

--- Source/Team4_CH4_Project/UI/HUD/SW_NotificationWidget.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "SW_NotificationWidget.h"
-#include "Components/Image.h"
-#include "Components/TextBlock.h"
-
--- Source/Team4_CH4_Project/UI/HUD/SW_SquadInfoWidget.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "SW_SquadInfoWidget.h"
-#include "Components/Image.h"
-#include "Components/ProgressBar.h"
-#include "Components/TextBlock.h"
