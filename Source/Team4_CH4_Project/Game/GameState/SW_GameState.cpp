a6ccef73df914d9008cdd07483aab7c748ae2c3b
nonecalculate <escortbayonet@gmail.com>
2025-04-11 09:41:53 +0900

    Update GameState

---
+++ Source/Team4_CH4_Project/Game/GameState/SW_GameState.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Team4_CH4_Project/Game/GameState/SW_GameState.h"
+
---
+++ Source/Team4_CH4_Project/Game/GameState/SW_GameState.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "SW_GameState.generated.h"
+
+UCLASS()
+class TEAM4_CH4_PROJECT_API ASW_GameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
