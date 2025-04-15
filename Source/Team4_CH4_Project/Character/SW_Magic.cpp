d6d12f988022167c0b3b243a8c50a45cda8d00cc
Chanyoung <parkcy723@naver.com>
2025-04-15 21:09:03 +0900

    플레이어컨트롤러에서 점프중일때는 점프어택만 가능, 땅이 발에 닿아있어야지만 스킬들이 나감

--- Source/Team4_CH4_Project/Character/SW_Magic.cpp
+++ Source/Team4_CH4_Project/Character/SW_Magic.cpp
@@ -5,52 +5,41 @@
 
 ASW_Magic::ASW_Magic()
 {
-	MoveSpeed = 1500.f;
-	CurrentComboIndex = -1; 
-	NormalStaticMesh = nullptr;
-	NormalSkillMaterial = nullptr;
-	NormalSkillNiagara = nullptr;
-	Combo1_M = nullptr;
-	Combo2_M = nullptr;
-	Combo3_M = nullptr;
-	ComboEffectNiagara = nullptr;
+    MoveSpeed = 1500.f;
 }
 
 void ASW_Magic::BeginPlay()
 {
-	Super::BeginPlay();
-	ApplyVisualSettings(); // BeginPlay에서도 설정 적용
+    Super::BeginPlay();
 }
-
 
 void ASW_Magic::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 }
 
 void ASW_Magic::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-	bool bFromSweep, const FHitResult& SweepResult)
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+    bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (!HasAuthority()) return;
+    if (!HasAuthority()) return;
 
-	if (!OtherActor || OtherActor == this || OtherActor == GetInstigator() || OtherActor == GetOwner())
-		return;
+    if (!OtherActor || OtherActor == this || OtherActor == GetInstigator() || OtherActor == GetOwner())
+        return;
 
-	if (AlreadyHitActors.Contains(OtherActor)) return;
-	AlreadyHitActors.Add(OtherActor);
+    if (AlreadyHitActors.Contains(OtherActor)) return;
+    AlreadyHitActors.Add(OtherActor);
 
-	UGameplayStatics::ApplyDamage(OtherActor, Damage, GetInstigatorController(), this, UDamageType::StaticClass());
+    UGameplayStatics::ApplyDamage(OtherActor, Damage, GetInstigatorController(), this, UDamageType::StaticClass());
 
-	Destroy();
+    Destroy();
 }
 
-// 나이아가라 생성 함수 분리 (선택적)
 void ASW_Magic::SpawnNiagaraEffect(UNiagaraSystem* NiagaraSystem)
 {
     UNiagaraFunctionLibrary::SpawnSystemAttached(
         NiagaraSystem,
-        MeshComp,
+        GetRootComponent(),
         NAME_None,
         FVector::ZeroVector,
         FRotator::ZeroRotator,
@@ -58,35 +47,3 @@ void ASW_Magic::SpawnNiagaraEffect(UNiagaraSystem* NiagaraSystem)
         true
     );
 }
-
-void ASW_Magic::ApplyVisualSettings()
-{
-    if (!MeshComp) return;
-
-    // 메시/머티리얼/나이아가라 설정 (기존 로직)
-    if (CurrentComboIndex >= 0) 
-    {
-        switch (CurrentComboIndex)
-        {
-        case 0: if (Combo1_M) MeshComp->SetMaterial(0, Combo1_M); break;
-        case 1: if (Combo2_M) MeshComp->SetMaterial(0, Combo2_M); break;
-        case 2: if (Combo3_M) MeshComp->SetMaterial(0, Combo3_M); break;
-        }
-
-        if (ComboEffectNiagara) SpawnNiagaraEffect(ComboEffectNiagara);
-
-        // 콤보 공격 크기 적용
-        SetActorScale3D(ComboScale); // 기본 콤보 크기
-
-    }
-    else // 노멀 스킬
-    {
-        if (NormalStaticMesh) MeshComp->SetStaticMesh(NormalStaticMesh);
-        if (NormalSkillMaterial) MeshComp->SetMaterial(0, NormalSkillMaterial);
-        if (NormalSkillNiagara) SpawnNiagaraEffect(NormalSkillNiagara);
-
-        // 노멀 스킬 크기 적용
-        SetActorScale3D(NormalSkillScale); 
-    }
-}
-
--- Source/Team4_CH4_Project/Character/SW_Magic.h
+++ Source/Team4_CH4_Project/Character/SW_Magic.h
@@ -9,59 +9,26 @@
 UCLASS()
 class TEAM4_CH4_PROJECT_API ASW_Magic : public ASW_ThrowActor
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	ASW_Magic();
+    ASW_Magic();
 
 protected:
-	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
-	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-		bool bFromSweep, const FHitResult& SweepResult) override;
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaTime) override;
+    virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+        bool bFromSweep, const FHitResult& SweepResult) override;
 
 public:
+    void SpawnNiagaraEffect(UNiagaraSystem* NiagaraSystem);
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic")
-	int32 CurrentComboIndex = -1;
+    // 콤보용 메시 스케일 설정
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Scale")
+    FVector ComboScale = FVector(2.5f);
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Mesh")
-	UStaticMesh* NormalStaticMesh;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Material")
-	UMaterialInterface* Combo1_M;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Material")
-	UMaterialInterface* Combo2_M;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Material")
-	UMaterialInterface* Combo3_M;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Material")
-	UMaterialInterface* NormalSkillMaterial;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Effect")
-	UNiagaraSystem* ComboEffectNiagara;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Effect")
-	UNiagaraSystem* NormalSkillNiagara;
-
-
-
-	// 나이아가라 이펙트 생성 함수
-	void SpawnNiagaraEffect(UNiagaraSystem* NiagaraSystem);
-
-
-	// 콤보어택 발사 메시크기설정용
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Scale")
-	FVector ComboScale = FVector(2.0f); 
-
-	// 노멀스킬 발사 메시크기설정용
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Scale")
-	FVector NormalSkillScale = FVector(2.0f);
-
-	// 머티/FX 적용용 함수
-	UFUNCTION(BlueprintCallable, Category = "Magic")
-	void ApplyVisualSettings(); 
+    // 노멀스킬용 메시 스케일 설정
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Magic|Scale")
+    FVector NormalSkillScale = FVector(2.0f);
 };
